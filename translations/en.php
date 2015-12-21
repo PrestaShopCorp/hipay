40a6550d1ffa7378408f4d9c2a7fc8e8f5207eb1
Johan Protin <jprotin@hipay.com>
2015-12-21 12:07:33 +0100

    adding translations

---
+++ translations/en.php
@@ -0,0 +1,25 @@
+<?php
+
+global $_MODULE;
+$_MODULE = array();
+$_MODULE['<{hipay}prestashop>hipay_17003c2df55bb0f4fa8b667cbee55271'] = 'What you have to do here:';
+$_MODULE['<{hipay}prestashop>hipay_1a762920de49c7b74aacd55eb8105cdf'] = 'Set your account data (account id, password and site id).';
+$_MODULE['<{hipay}prestashop>hipay_c1e325ca380940778396b2a59a12c8c8'] = 'Select order category and age classification.';
+$_MODULE['<{hipay}prestashop>hipay_0d9bc09d013ab46685bf4906f037be90'] = 'Set an e-mail address for payment notifications.';
+$_MODULE['<{hipay}prestashop>hipay_bb170269b2c16f6abd8a9b0770e2e0bf'] = 'For more informations please go to the How to setup Hipay tab above.';
+$_MODULE['<{hipay}prestashop>hipay_442b517266abda68d34186308f6688d1'] = 'ID number of the Hipay account on which this store website is declared.';
+$_MODULE['<{hipay}prestashop>hipay_7df859fa42824ededc9fe6be4a2c12fd'] = 'Normally this is your main account. Do not use your member area id here!';
+$_MODULE['<{hipay}prestashop>hipay_1d482d4c60244390e9379159e1d918dc'] = 'Merchant password of the Hipay account on which this store website is declared (it\'s not the login password!).';
+$_MODULE['<{hipay}prestashop>hipay_6af92815c2e2a9625e9c6924c91c14d9'] = 'To set a new merchant password: Log in to your Hipay account, navigate to Payment buttons where you can find a list of your registered sites.';
+$_MODULE['<{hipay}prestashop>hipay_6fb4df027873f9bceb153ef44937279f'] = 'Click on the site informations button of the related site.';
+$_MODULE['<{hipay}prestashop>hipay_21a668545a28d1f34274e72e2b9609ed'] = 'Enter your merchant password and click on confirm. ';
+$_MODULE['<{hipay}prestashop>hipay_0a3e0fd0d6a6e3dd3aefc66de3a50b3e'] = 'Do not forget to enter your new password here too.';
+$_MODULE['<{hipay}prestashop>hipay_345362c750a14e2750390dd48a2eb395'] = 'Id of the selected site.';
+$_MODULE['<{hipay}prestashop>hipay_8bac8ee80a53b5d1d5713b2c4f830698'] = 'To get a site id register your store website on the selected Hipay account.';
+$_MODULE['<{hipay}prestashop>hipay_557fb9486f21c5d3d3dcb1381dc2d1e5'] = 'You can find this option in your Hipay account under the menu item Payment buttons.';
+$_MODULE['<{hipay}prestashop>hipay_5b6f03c7676b51bef4baef5b36211155'] = 'Select the order category.';
+$_MODULE['<{hipay}prestashop>hipay_e83125cbc6cd8bb839e7fe95a4aea213'] = 'A list of your categories (based on the selected business lineand category for your site on Hipay).';
+$_MODULE['<{hipay}prestashop>hipay_b9a8391f806b753146a9c627c0f2f8ea'] = '1. Enter your Site Id';
+$_MODULE['<{hipay}prestashop>hipay_33395e45ede2fad8084c8f6df9aed43f'] = '2. Click on Save Config';
+$_MODULE['<{hipay}prestashop>hipay_ba4798793aa5b70c18b5e08501fda408'] = '3. The Order Category list will be updated.';
+$_MODULE['<{hipay}prestashop>hipay_30dcf9ebea707ea1eedfdd0c80592dfd'] = '4. Choose a pleasant category and click on Save Config again.';
