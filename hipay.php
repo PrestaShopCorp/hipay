6af116c5510337e486f149303be2dc607408daf9
Johan Protin <jprotin@hipay.com>
2016-06-28 16:08:23 +0200

    Update and optimization Prestashop validator

--- hipay.php
+++ hipay.php
@@ -1,5 +1,19 @@
 <?php
-//include(dirname(__FILE__).'/../../config/config.inc.php');
+/**
+ * HiPay 
+ *
+ * NOTICE OF LICENSE
+ *
+ * This source file is subject to the Apache 2.0 License
+ * that is bundled with this package in the file LICENSE.txt.
+ * It is also available through the world-wide-web at this URL:
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * @author HiPay <support.wallet@hipay.com>
+ * @copyright Copyright (c) 2016 - HiPay
+ * @license http://www.apache.org/licenses/LICENSE-2.0 Apache 2.0 License
+ */
+
 if (_PS_VERSION_ < '1.5') {
 	// version 1.4
 	require_once(_PS_ROOT_DIR_.'/modules/hipay/hipay-14.php');
--- upgrade/upgrade-1.6.14.php
+++ upgrade/upgrade-1.6.14.php
@@ -1,4 +1,19 @@
 <?php
+/**
+ * HiPay 
+ *
+ * NOTICE OF LICENSE
+ *
+ * This source file is subject to the Apache 2.0 License
+ * that is bundled with this package in the file LICENSE.txt.
+ * It is also available through the world-wide-web at this URL:
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * @author HiPay <support.wallet@hipay.com>
+ * @copyright Copyright (c) 2016 - HiPay
+ * @license http://www.apache.org/licenses/LICENSE-2.0 Apache 2.0 License
+ */
+
 if (!defined('_PS_VERSION_'))
     exit;
 
